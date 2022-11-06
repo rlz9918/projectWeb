a88c4ddaef50b8ff55f77c3c4ff7cbb903bb9a5a
Rodrigo Alonso <rodrigoalonso2000@gmail.com>
2022-11-05 23:10:51 -0300

    form style

--- src/pages/footer/footer.jsx
+++ src/pages/footer/footer.jsx
@@ -9,21 +9,25 @@ export default function Footer() {
     return (
         <>
             <footer class='footer' id='newsLetter'>
+                <div class="footer-content-container">
                 <img src={`${newsLetter}`} alt='news-letter' class='newsLetter'></img>
                 <Typography className="footer-text">Accedé a mi contenido de news letter
                     para poder adquirir videos de lirics y canciones que no saque aún</Typography>
+                <div class="form-container">
                 <Box
                     component="form"
                     sx={{
-                        '& > :not(style)': { m: 1, width: '25ch' },
+                        '& > :not(style)': { m: 1, width: '35ch'},
                     }}
                     noValidate
                     autoComplete="off"
+                    
                 >
                     <TextField id="outlined-basic" label="Email" variant="outlined" />
-                    <Button>Enviar</Button>
+                    <Button className='MuiButtonBase' variant="outlined">Enviar</Button>
                 </Box>
-
+                </div>
+                </div>
             </footer>
         </>
     );
